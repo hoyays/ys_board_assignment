9bb72fbc374ec1d99cdd0aedc1f657bbcdae7403
hoyays <hoyays@naver.com>
2021-07-23 17:53:22 +0900

    7월 23일/WebSocket connection 완료

---
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.example.demo.handler.ChatWebSocketHandler;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new ChatWebSocketHandler(), "/chat").setAllowedOrigins("*");
+	}
+	
+	
+	
+
+}//class
